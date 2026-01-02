1e540068b08a2cc0135a3a25473c75ee5b7185ec
Michał Górny <mgorny@quansight.com>
2026-01-02 16:44:46 +0100

    Create a real function to test
    Signed-off-by: Michał Górny <mgorny@quansight.com>

---
+++ common/limited.c
@@ -0,0 +1,30 @@
+#include <Python.h>
+
+#ifndef Py_LIMITED_API
+#error Py_LIMITED_API must be defined.
+#endif
+
+static PyObject *
+add(PyObject * Py_UNUSED(self), PyObject *args) {
+    long a, b;
+
+    if (!PyArg_ParseTuple(args, "ll", &a, &b))
+        return NULL;
+
+    return PyLong_FromLong(a + b);
+}
+
+static struct PyMethodDef methods[] = {
+    { "add", add, METH_VARARGS, NULL },
+    { NULL, NULL, 0, NULL },
+};
+
+static PyModuleDef_Slot limited_module_slots[] = {
+    {Py_mod_name, "limited"},
+    {Py_mod_methods, methods},
+    {0}
+};
+
+PyMODEXPORT_FUNC PyModExport_limited(void) {
+    return limited_module_slots;
+}
--- meson-python/c/test_limited.py
+++
@@ -1,8 +0,0 @@
-from limited import hello
-
-
-def test_hello():
-    assert hello() == "hello world"
-
-
-test_hello()
