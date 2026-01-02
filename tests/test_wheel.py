c8b2dd019dd1ddf71920204caf4f95730b30abd8
Michał Górny <mgorny@quansight.com>
2026-01-02 13:37:19 +0100

    Use `build` to build the wheel in non-isolated env
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2025 Michał Górny
 # SPDX-License-Identifier: MIT
 
+from build import ProjectBuilder
 
 import typing
 
@@ -8,5 +9,7 @@ if typing.TYPE_CHECKING:
     from pathlib import Path
 
 
-def test_install(test_case: Path, venv) -> None:
-    venv.pip("install", str(test_case))
+def test_install(test_case: Path, tmp_path: Path, venv) -> None:
+    builder = ProjectBuilder(test_case)
+    dist_path = builder.build("wheel", tmp_path)
+    venv.pip("install", dist_path)
