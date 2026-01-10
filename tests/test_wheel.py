8c2265584ee9f6d57d9085c36f6d3b805b5276e9
Michał Górny <mgorny@quansight.com>
2026-01-10 16:55:06 +0100

    Test that .abi3 extension is actually created
    Signed-off-by: Michał Górny <mgorny@quansight.com>

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -13,6 +13,10 @@ XFAIL_CYTHON = pytest.mark.xfail(
     IS_FREETHREADING,
     reason="https://github.com/cython/cython/issues/7399#issuecomment-3710960697",
 )
+XFAIL_NANOBIND = pytest.mark.xfail(
+    IS_FREETHREADING,
+    reason="nanobind does not support PEP 803 yet",
+)
 XFAIL_PYO3 = pytest.mark.xfail(
     IS_FREETHREADING,
     reason="pyo3 does not support 3.15 (or PEP 803) yet",
@@ -25,10 +29,26 @@ TEST_CASES = [
     pytest.param("meson-python/cython", marks=[XFAIL_CYTHON]),
     "scikit-build-core/c",
     pytest.param("scikit-build-core/cython", marks=[XFAIL_CYTHON]),
-    "scikit-build-core/nanobind",
+    pytest.param("scikit-build-core/nanobind", marks=[XFAIL_NANOBIND]),
     "setuptools/c",
     pytest.param("setuptools/cython", marks=[XFAIL_CYTHON]),
 ]
+
+TEST_PROGRAM = """
+from pathlib import Path
+
+import limited
+
+# if the build system created a package, import the actual extension
+if hasattr(limited, "__path__"):
+    from limited import limited
+
+value = limited.add(1, 2)
+assert value == 3, f"add(1, 2) == {value} instead of 3"
+
+assert ".abi3" in Path(limited.__file__).name, (
+    f"{limited.__file__} is not abi3 extension")
+"""
 
 
 @pytest.mark.parametrize("test_case", TEST_CASES)
@@ -37,4 +57,4 @@ def test_install(test_case: str, tmp_path: Path, venv) -> None:
     dist_path = builder.build("wheel", tmp_path)
     venv.pip("install", dist_path)
     # -Werror to catch the exception when extension is not freethreading-compatible
-    venv.python("-Werror", "-c", "import limited; assert limited.add(1, 2) == 3")
+    venv.python("-Werror", "-c", TEST_PROGRAM)
