f8fe4f28b62876422674a129f4f173fb10cba332
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-02-20 13:30:16 -0700

    Use a setup.py and setup.cfg file to work around setuptools static configuration limitations

---
+++ setuptools/cython/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+    packages = find_packages(
+        where="src",
+        include = ["limited*"],
+    ),
+    ext_modules = [Extension(
+        name = "limited",
+        sources = ["src/limited/limited.pyx"],
+        py_limited_api = True,
+        extra_compile_args = [
+            "-D_Py_OPAQUE_PYOBJECT",
+            "-DPy_LIMITED_API=0x030f0000",
+        ],
+    ),],
+)
